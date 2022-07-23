e0ae78b32816963738f635da699be66465535dd1
Noah Celuch <noah.celuch@gmail.com>
2022-07-23 13:41:04 -0400

    feat: init commit

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+    res.json({ message: 'The games microservice for The Ultmt API' })
+})
+
+export default app
