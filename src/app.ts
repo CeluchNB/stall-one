053899f578c5d4b15c95a106126a77f0ccc2a7e4
Noah Celuch <noah.celuch@gmail.com>
2022-07-28 21:06:01 -0400

    ci: dockerized and added github actions

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,11 @@
 import express from 'express'
 import cors from 'cors'
-import Game from './models/game'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 app.get('/', async (req, res) => {
-    await Game.create({ home: 'Hazard', away: 'Bomb Squad' })
     res.json({ message: 'The games microservice for The Ultmt API' })
 })
 
