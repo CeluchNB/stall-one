7ed1bc3571a4fb2427bf49761da48d0047b0f5ce
Noah Celuch <noah.celuch@gmail.com>
2022-08-22 21:36:31 -0400

    fix: updated base endpoint to use axios

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import passport from 'passport'
 import { router as v1Router } from '../src/routes/v1'
+import axios from 'axios'
 
 const app = express()
 app.use(cors())
@@ -13,10 +14,10 @@ require('./loaders/passport')
 app.use('/api/v1', v1Router)
 
 app.get('/stall-one', async (req, res) => {
-    const response = await fetch(`${process.env.ULTMT_API_URL}/ultmt`, {
+    const response = await axios.get(`${process.env.ULTMT_API_URL}/ultmt`, {
         headers: { 'x-api-key': process.env.API_KEY || '' },
     })
-    const message = await response.json()
+    const message = response.data
     res.json({ message: message.message })
 })
 
