e1e589d741f0ca701caa093581b330b939d453d3
Noah Celuch <noah.celuch@gmail.com>
2024-01-02 23:38:56 -0500

    ci: test remove swc

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
-console.log('Very first thing', Date.now())
 import { setupApp } from './app'
 
 setupApp().then((app) => {
     const PORT = process.env.PORT || 3000
 
     app.listen(PORT, () => {
-        console.log('Listening', Date.now())
         return console.log('Stall one listening on', PORT)
     })
 })
