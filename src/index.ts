8504259987f99cf25a25ae9666f342c801be2a8d
Noah Celuch <noah.celuch@gmail.com>
2024-01-06 21:50:10 -0500

    chore: revert to ts node for running

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
-console.log('very first thing', Date.now())
 import { setupApp } from './app'
 
 setupApp().then((app) => {
     const PORT = process.env.PORT || 3000
 
     app.listen(PORT, () => {
-        console.log('listening', Date.now())
         return console.log('Stall one listening on', PORT)
     })
 })
