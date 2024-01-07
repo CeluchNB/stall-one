a07b1a654ac8fd1f7f9bd5c7303b336e5efc4d70
Noah Celuch <noah.celuch@gmail.com>
2024-01-06 20:11:28 -0500

    chore: console timing statements

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
+console.log('Very first thing', Date.now())
 import { setupApp } from './app'
 
 setupApp().then((app) => {
     const PORT = process.env.PORT || 3000
 
     app.listen(PORT, () => {
+        console.log('listening', Date.now())
         return console.log('Stall one listening on', PORT)
     })
 })
