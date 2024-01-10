ac3e72ffff577a7fa72d6099ca4ada16821b7b60
Noah Celuch <noah.celuch@gmail.com>
2024-01-09 20:32:15 -0500

    wip(log): extra logging data

--- src/logging/index.ts
+++ src/logging/index.ts
@@ -25,6 +25,9 @@ export const Logger = () => {
                 requestUrl: req.url,
                 requestMethod: req.method,
             },
+            body: req.body,
+            params: req.params,
+            query: req.query,
         })
         next()
     }
